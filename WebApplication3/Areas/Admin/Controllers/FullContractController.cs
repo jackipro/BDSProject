8980eb0df245d5acca58fe962d50bf7ae203c326
Nam <nam.t171846@vanlanguni.vn>
2019-11-11 21:43:25 +0700

    Create view list, add, update fullcontract

---
+++ WebApplication3/Areas/Admin/Controllers/FullContractController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Areas.Admin.Controllers
+{
+    public class FullContractController : Controller
+    {
+		// GET: Admin/FullContract
+		QUANLYBDSEntities model = new QUANLYBDSEntities();
+		public ActionResult Index()
+
+        {
+			var fullcontract = model.Full_Contract.OrderByDescending(x => x.ID).ToList();
+            return View(fullcontract);
+        }
+		[HttpGet]
+		public ActionResult Create()
+		{
+			ViewBag.property = model.Properties.OrderByDescending(x => x.ID).ToList();
+			return View();
+		}
+		[HttpPost]
+		public ActionResult Create(Full_Contract c)
+		{
+			var contract = new Full_Contract();
+			contract.FullContractCode = c.FullContractCode;
+			contract.CustomerName = c.CustomerName;
+			contract.YearOfBirth = c.YearOfBirth;
+			contract.SSN = c.SSN;
+			contract.CustomerAddress = c.CustomerAddress;
+			contract.Mobile = c.Mobile;
+			contract.DateOfContract = c.DateOfContract;
+			contract.Price = c.Price;
+			contract.Deposit = c.Deposit;
+			contract.Remain = c.Remain;
+			contract.Status = c.Status;
+			contract.PropertyID = c.PropertyID;
+			model.Full_Contract.Add(contract);
+			model.SaveChanges();
+			return RedirectToAction("Index");
+		}
+		[HttpGet]
+		public ActionResult Edit(int id)
+		{
+			var fullcontract = model.Full_Contract.FirstOrDefault(x => x.ID == id);
+			ViewBag.property = model.Properties.OrderByDescending(x => x.ID).ToList();
+			return View(fullcontract);
+		}
+		[HttpPost]
+		public ActionResult Edit(int id, Full_Contract c)
+		{
+			var contract = model.Full_Contract.FirstOrDefault(x => x.ID == id);
+			contract.FullContractCode = c.FullContractCode;
+			contract.CustomerName = c.CustomerName;
+			contract.YearOfBirth = c.YearOfBirth;
+			contract.SSN = c.SSN;
+			contract.CustomerAddress = c.CustomerAddress;
+			contract.Mobile = c.Mobile;
+			contract.DateOfContract = c.DateOfContract;
+			contract.Price = c.Price;
+			contract.Deposit = c.Deposit;
+			contract.Remain = c.Remain;
+			contract.Status = c.Status;
+			contract.PropertyID = c.PropertyID;
+			model.SaveChanges();
+			return RedirectToAction("Index");
+		}
+
+	}
+}
--- WebApplication3/Models/Model1.Designer.cs
+++ WebApplication3/Models/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'E:\Learning\HỌC KỲ 1 NĂM 3\WebApplication3\WebApplication3\Models\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'E:\Learning\hoc ky 1 nam 3\WebApplication3\WebApplication3\Models\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
